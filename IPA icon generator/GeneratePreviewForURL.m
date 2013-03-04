6850392f6ba91da55783c1b77851ef47ed489ec7
Peter Hosey <git@boredzo.org>
2013-03-03 19:57:27 -0800

    Initial Commit

---
+++ IPA icon generator/GeneratePreviewForURL.m
@@ -0,0 +1,19 @@
+OSStatus GeneratePreviewForURL(void *thisInterface, QLPreviewRequestRef preview, CFURLRef url, CFStringRef contentTypeUTI, CFDictionaryRef options);
+void CancelPreviewGeneration(void *thisInterface, QLPreviewRequestRef preview);
+
+/* -----------------------------------------------------------------------------
+   Generate a preview for file
+
+   This function's job is to create preview for designated file
+   ----------------------------------------------------------------------------- */
+
+OSStatus GeneratePreviewForURL(void *thisInterface, QLPreviewRequestRef preview, CFURLRef url, CFStringRef contentTypeUTI, CFDictionaryRef options)
+{
+    // To complete your generator please implement the function GeneratePreviewForURL in GeneratePreviewForURL.c
+    return noErr;
+}
+
+void CancelPreviewGeneration(void *thisInterface, QLPreviewRequestRef preview)
+{
+    // Implement only if supported
+}
